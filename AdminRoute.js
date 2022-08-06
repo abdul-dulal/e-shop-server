26d06ff7cc5ab0f998d90dc46907d34d9301bdee
abdul-dulal <dulal.dpi.387364@gmail.com>
2022-08-06 11:59:34 +0600

    m

--- AdminRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const adminRoute = express.Router();
-
-adminRoute.get("/login", (req, res) => {
-  res.send("hello from router");
-});
-module.exports = adminRoute;
